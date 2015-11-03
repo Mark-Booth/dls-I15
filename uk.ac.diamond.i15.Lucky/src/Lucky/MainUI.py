71e8a7ef89e5b5ae61d86a7e9888b13ae7ac2012
Michael Wharmby <michael.wharmby@diamond.ac.uk>
2015-11-03 18:09:24 +0000

    Adds two more button objects to UI

--- uk.ac.diamond.i15.Lucky/src/Lucky/MainUI.py
+++ uk.ac.diamond.i15.Lucky/src/Lucky/MainUI.py
@@ -4,11 +4,34 @@ Created on 3 Nov 2015
 @author: wnm24546
 '''
 
-from PyQt4 import QtGui
+from PyQt4 import QtCore, QtGui
+import UIApplicationModel
 
 class LuckyMainWindow(QtGui.QMainWindow):
     def __init__(self):
         super(LuckyMainWindow, self).__init__()
+        luckyAppModel = UIApplicationModel.ApplicationModel()
         
         self.setWindowTitle('Lucky')
         #self.SetWindowIcon(QtGui.QIcon('SomeLocalIcon.png'))
+        
+        #Control buttons
+        runBtn = QtGui.QPushButton('Run', self)
+        runBtn.clicked.connect(self.runBtnClicked())
+        
+        stopBtn = QtGui.QPushButton('Stop', self)
+        stopBtn.clicked.connect(self.stopBtnClicked())
+        
+        quitBtn = QtGui.QPushButton('Quit', self)
+        quitBtn.clicked.connect(QtCore.QCoreApplication.instance().quit)
+        
+        
+        
+        
+    def runBtnClicked(self):
+        sender = self.sender()
+        sender.text()
+    
+    def stopBtnClicked(self):
+        sender = self.sender()
+        sender.text()

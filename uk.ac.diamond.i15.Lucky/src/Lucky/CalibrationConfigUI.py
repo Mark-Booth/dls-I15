9c51379a18fad47ebd684e087266c96e45a8073c
Michael Wharmby <michael.wharmby@diamond.ac.uk>
2015-11-04 17:39:32 +0000

    Initial commit of custom calibration configuration dialog box

---
+++ uk.ac.diamond.i15.Lucky/src/Lucky/CalibrationConfigUI.py
@@ -0,0 +1,19 @@
+'''
+Created on 4 Nov 2015
+
+@author: wnm24546
+'''
+from PyQt4.QtGui import (QDialog, QDialogButtonBox)
+
+class CalibrationConfigUI(QDialog):
+    def __init(self,parent=None):
+        super(CalibrationConfigUI, self).__init__(parent)
+        self.setWindowTitle("Calibration Configuration")
+        
+        self.setupUI()
+    
+    def setupUI(self):
+        okCancelBtnBox = QDialogButtonBox(QDialogButtonBox.Ok | QDialogButtonBox.Cancel)
+        okCancelBtnBox.accepted.connect(self.okClick)
+        okCancelBtnBox.rejected.connect(self.cancelClick)
+        #layout.addWidget(okCancelBtnBox)

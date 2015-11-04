b3d9991f932f50fce26eba9a1065ad5d20891f8f
Michael Wharmby <michael.wharmby@diamond.ac.uk>
2015-11-04 00:14:11 +0000

    Adds layout to buttons. Changes to a QWidget

--- uk.ac.diamond.i15.Lucky/src/Lucky/LuckyUIView.py
+++ uk.ac.diamond.i15.Lucky/src/Lucky/LuckyUIView.py
@@ -7,32 +7,44 @@ Created on 3 Nov 2015
 from PyQt4 import QtCore, QtGui
 from Lucky import LuckyUIModel
 
-class MainWindow(QtGui.QMainWindow):
+class MainWindow(QtGui.QWidget):    
     def __init__(self):
         super(MainWindow, self).__init__()
+        self.initUI()
+        
+    def initUI(self):
         luckyAppModel = LuckyUIModel.MainWindowModel()
         
         self.setWindowTitle('Lucky')
         #self.SetWindowIcon(QtGui.QIcon('SomeLocalIcon.png'))
         
         #Control buttons
-        runBtn = QtGui.QPushButton('Run', self)
+        runBtn = QtGui.QPushButton('Run')
         runBtn.clicked.connect(self.runBtnPressed)
         
-        stopBtn = QtGui.QPushButton('Stop', self)
+        stopBtn = QtGui.QPushButton('Stop')
         stopBtn.clicked.connect(self.stopBtnPressed)
         
-        quitBtn = QtGui.QPushButton('Quit', self)
+        quitBtn = QtGui.QPushButton('Quit')
         quitBtn.clicked.connect(QtCore.QCoreApplication.instance().quit)
         
-    def runBtnPressed():
+        #Layout
+        hBoxLayout = QtGui.QHBoxLayout()
+        hBoxLayout.addWidget(runBtn)
+        hBoxLayout.addWidget(stopBtn)
+        hBoxLayout.addWidget(quitBtn)
+        
+        self.updateWidgetStates()
+        self.setLayout(hBoxLayout)
+        
+    def runBtnPressed(self):
     	luckyAppModel.runLuckyCalcs()
     	self.updateWidgetStates()
     
-    def stopBtnPressed():
+    def stopBtnPressed(self):
     	luckyAppModel.stopLuckyCalcs()
     	self.updateWidgetStates()
     
-    def updateWidgetStates():
+    def updateWidgetStates(self):
     	runBtn.setEnabled(luckyAppModel.runEnabled)
     	stopBtn.setEnabled(luckyAppModel.stopEnabled)

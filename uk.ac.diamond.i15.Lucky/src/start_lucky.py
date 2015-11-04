59843e73cedf0090070416632167d800c1d07a0d
Michael Wharmby <michael.wharmby@diamond.ac.uk>
2015-11-04 17:40:20 +0000

    Updated lucky launcher (for development).

--- uk.ac.diamond.i15.Lucky/src/start_lucky.py
+++ uk.ac.diamond.i15.Lucky/src/start_lucky.py
@@ -8,10 +8,14 @@ import sys
 from PyQt4 import QtGui
 
 import Lucky.LuckyUIView
+import Lucky.CalibrationConfigUI
+import Lucky.MainUI
 
 def main():
     app = QtGui.QApplication(sys.argv)
-    lucky = Lucky.LuckyUIView.MainWindow()
+    lucky0 = Lucky.LuckyUIView.MainWindow()
+    lucky = Lucky.MainUI.MainUI()
+#    lucky0.show()
     lucky.show()
     sys.exit(app.exec_())
 

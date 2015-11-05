0a7da2fb9c182e1a61f3eed678a9438f71a8bfcc
Michael Wharmby <michael.wharmby@diamond.ac.uk>
2015-11-05 16:50:18 +0000

    Updates to reflect renamed main UI class.

--- uk.ac.diamond.i15.Lucky/src/start_lucky.py
+++ uk.ac.diamond.i15.Lucky/src/start_lucky.py
@@ -9,12 +9,12 @@ from PyQt4 import QtGui
 
 import Lucky.LuckyUIView
 import Lucky.CalibrationConfigUI
-import Lucky.MainUI
+import Lucky.MainView
 
 def main():
     app = QtGui.QApplication(sys.argv)
     lucky0 = Lucky.LuckyUIView.MainWindow()
-    lucky = Lucky.MainUI.MainUI()
+    lucky = Lucky.MainView.MainView()
 #    lucky0.show()
     lucky.show()
     sys.exit(app.exec_())

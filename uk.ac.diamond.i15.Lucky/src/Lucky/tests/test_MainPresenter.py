65633f5906c450349ab6be052450954a53c86f5b
Michael Wharmby <michael.wharmby@diamond.ac.uk>
2015-11-05 17:32:35 +0000

    Initial commit

---
+++ uk.ac.diamond.i15.Lucky/src/Lucky/tests/test_MainPresenter.py
@@ -0,0 +1,12 @@
+'''
+Created on 5 Nov 2015
+
+@author: wnm24546
+'''
+import unittest
+import Lucky
+
+class MainPresenterTest(unittest.TestCase):
+    def setUp(self):
+        mp = Lucky.MainPresenter.MainPresenter()
+    

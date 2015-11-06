f48a2120d156f3d66d17eb591abd81c462a9d05b
Michael Wharmby <michael.wharmby@diamond.ac.uk>
2015-11-06 12:02:40 +0000

    Added code to flesh out functionality (still doesn't work properly though)

--- uk.ac.diamond.i15.Lucky/src/Lucky/CalibrationConfigView.py
+++ uk.ac.diamond.i15.Lucky/src/Lucky/CalibrationConfigView.py
@@ -3,25 +3,34 @@ Created on 4 Nov 2015
 
 @author: wnm24546
 '''
-from PyQt4.QtGui import (QDialog, QDialogButtonBox)
+from PyQt4 import QtCore, QtGui
+from PyQt4.QtGui import (QDialog, QDialogButtonBox, QVBoxLayout,
+                         QLabel, QHBoxLayout, QWidget, QLineEdit, QPushButton)
+
+from PyQt4.QtCore import Qt
+
 
 class CalibrationConfigView(QDialog):
     def __init(self,parent=None):
-        super(CalibrationConfigView, self).__init__(parent)
+#         super(CalibrationConfigView, self).__init__(self, parent) #This doesn't work.
+        QDialog.__init__(self, parent)
         self.setWindowTitle("Calibration Configuration")
         
         self.setupUI()
-        
-#        self.show()
-    
+     
     def setupUI(self):
+        baseLayout = QVBoxLayout()
+         
         okCancelBtnBox = QDialogButtonBox(QDialogButtonBox.Ok | QDialogButtonBox.Cancel)
         okCancelBtnBox.accepted.connect(self.okClick)
         okCancelBtnBox.rejected.connect(self.cancelClick)
-        #layout.addWidget(okCancelBtnBox)
-    
+        baseLayout.addWidget(okCancelBtnBox)
+         
+        self.addLayout(baseLayout)
+     
     def okClick(self):
-        pass
-    
+        print "OK!"
+         
+     
     def cancelClick(self):
-        pass
+        print "Cancel"

2221cd48df0d463c64f42348b7e4be2cd5660d22
Michael Wharmby <michael.wharmby@diamond.ac.uk>
2015-11-09 23:25:00 +0000

    Tidied up definition of CalibrationConfigData & renamed both classes.

--- uk.ac.diamond.i15.Lucky/src/Lucky/DataModel.py
+++ uk.ac.diamond.i15.Lucky/src/Lucky/DataModel.py
@@ -4,7 +4,7 @@ Created on 9 Nov 2015
 @author: wnm24546
 '''
 
-class MainViewData(object):
+class MainData(object):
     def __init__(self, mode=(1,0), calibType=(1,0,0), dataDir=None,
                  usdsPair=0, integStart=0, integEnd=0, integDelta=0):
         self.mode = mode
@@ -13,17 +13,14 @@ class MainViewData(object):
         self.usdsPair = usdsPair
         self.integrationConf = [integStart, integEnd, integDelta]
 
-class CalibrationConfigViewData(object):
+class CalibrationConfigData(object):
     def __init__(self, calibDir=None, bulbTemp=0,
                  calibUS=None, calibDS=None, 
                  calibF1US=None, calibF1DS=None,
                  calibF2US=None, calibF2DS=None):
         self.calibDir = calibDir
         self.bulbTemp = bulbTemp
-        self.calibUS = calibUS
-        self.calibDS = calibDS
-        self.calibF1US = calibF1US
-        self.calibF1DS = calibF1DS
-        self.calibF2US = calibF2US
-        self.calibF2DS = calibF2DS
+        self.calib = [calibUS, calibDS]
+        self.calibF1 = [calibF1US, calibF1DS] 
+        self.calibF2 = [calibF2US, calibF2DS]
         

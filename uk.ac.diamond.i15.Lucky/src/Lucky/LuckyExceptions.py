cfa5da3bd996aa53d005ae195e9e45495b216929
Michael Wharmby <michael.wharmby@diamond.ac.uk>
2015-11-10 00:53:10 +0000

    Initial commit

---
+++ uk.ac.diamond.i15.Lucky/src/Lucky/LuckyExceptions.py
@@ -0,0 +1,9 @@
+'''
+Created on 10 Nov 2015
+
+@author: wnm24546
+'''
+
+class BadModelStateException(Exception):
+    def __init__(self, message):
+        super(BadModelStateException, self).__init__(message)

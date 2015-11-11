5a1633bf4117e73a6b0d3357633844d4db46d4f1
Michael Wharmby <michael.wharmby@diamond.ac.uk>
2015-11-11 16:41:03 +0000

    Adds basic statemachine implementation.

--- uk.ac.diamond.i15.Lucky/src/Lucky/MainPresenter.py
+++ uk.ac.diamond.i15.Lucky/src/Lucky/MainPresenter.py
@@ -4,6 +4,26 @@ Created on 5 Nov 2015
 @author: wnm24546
 '''
 
+from MPStates import State
+
 class MainPresenter(object):
     def __init__(self):
         pass
+
+class StateMachine(object):
+    def __init__(self):
+        #This is a slightly convoluted way to avoid importing StartState
+        self.currentState = State().next(State.EVENTS.START)()
+        self.currentState.run()
+        self.changeState(State.EVENTS.LIVE)
+        
+    def changeState(self, event):
+        while True:
+            nextState = self.currentState.next(event)
+            if nextState == self.currentState:
+                break
+            self.currentState = nextState()
+            self.currentState.run()
+    
+    def getState(self):
+        return self.currentState.name

b799bb7aec31ad9f9c12fcd074fa92206ce57911
Michael Wharmby <michael.wharmby@diamond.ac.uk>
2015-11-11 17:57:33 +0000

    Completes LiveSetupTest

--- uk.ac.diamond.i15.Lucky/test/Lucky/test_MPStates.py
+++ uk.ac.diamond.i15.Lucky/test/Lucky/test_MPStates.py
@@ -7,7 +7,7 @@ Created on 10 Nov 2015
 import unittest
 
 from Lucky.DataModel import MainData
-from Lucky.MPStates import (LiveSetup, LiveStartable, LiveStoppable,
+from Lucky.MPStates import (State, LiveSetup, LiveStartable, LiveStoppable,
                             OfflineSetup, OfflineStartable, OfflineStoppable)
 
 class MPStateTest(unittest.TestCase):
@@ -20,6 +20,10 @@ class MPStateTest(unittest.TestCase):
     
     def stateRun(self):
         self.state.run(self.dM)
+    
+    def compareTransitions(self, expected):
+        presentTransitions = self.state.transitions.keys()
+        self.assertTrue(all(pt in expected for pt in presentTransitions), "Extra transition present!")
 
 class LiveSetupTest(MPStateTest):
     def runTest(self):
@@ -29,6 +33,12 @@ class LiveSetupTest(MPStateTest):
         self.dM.runEnabled = True
         
         self.state = LiveSetup()
+        
+        #Check the correct transitions are present
+        expectedTransitions = [State.EVENTS.DATAGOOD, State.EVENTS.OFFLINE]
+        self.compareTransitions(expectedTransitions)
+        
+        #Do the state's action
         self.stateRun()
         
         self.assertEqual(self.dM.mode, (1, 0), "Live mode not set")

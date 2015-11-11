b3e73efd1a727ca583fee03b3f955f69b01e2549
Michael Wharmby <michael.wharmby@diamond.ac.uk>
2015-11-11 17:16:11 +0000

    Adds missing offline events and fleshes out implementation of

--- uk.ac.diamond.i15.Lucky/src/Lucky/MPStates.py
+++ uk.ac.diamond.i15.Lucky/src/Lucky/MPStates.py
@@ -13,9 +13,8 @@ class State(object):
         self.name = None
         self.transitions = {State.EVENTS.START : StartState}
     
-    def run(self):
+    def run(self, dataModel):
         assert 0, "Run not implemented"
-        return True
     
     def next(self, event):
         if event in self.transitions:
@@ -30,23 +29,99 @@ class StartState(State):
         self.transitions = {State.EVENTS.LIVE : LiveSetup}
     
     def run(self):
-        print "I am in start state"
+        pass
 
-class LiveSetup(State):
+#Live state classes
+####
+class LiveState(State):
+    def __init__(self):
+        super(LiveState, self).__init__()
+    
+    def run(self, dataModel):
+        dataModel.mode = (1, 0)
+        dataModel.usdsControlsEnabled = False
+
+class LiveSetup(LiveState):
     def __init__(self):
         super(LiveSetup, self).__init__()
         self.name = "LiveSetup"
-        self.transitions = {State.EVENTS.DATAGOOD : LiveStartable}
+        self.transitions = {State.EVENTS.DATAGOOD : LiveStartable,
+                            State.EVENTS.OFFLINE  : OfflineSetup}
     
-    def run(self):
-        self.next(self.EVENTS.DATAGOOD)
+    def run(self, dataModel):
+        super(LiveSetup, self).run(dataModel)
+        dataModel.runEnabled = False
 
-class LiveStartable(State):
-    def __init__(self):
-        super(State, self).__init__()
+class LiveStartable(LiveState):
+    def __init__(self, dataModel):
+        super(LiveStartable, self).__init__()
         self.name = "LiveStartable"
-        self.transitions = {State.EVENTS.DATABAD : LiveSetup}
+        self.transitions = {State.EVENTS.DATABAD : LiveSetup,
+                            State.EVENTS.OFFLINE : OfflineSetup,
+                            State.EVENTS.RUN     : LiveStoppable}
     
-    def run(self):
-        print "I am in start mode"
+    def run(self, dataModel):
+        super(LiveStartable, self).run(dataModel)
+        self.dataModel.runEnabled = True
+        self.dataModel.stopEnabled = False
+        self.allUIControlsEnabled = False
 
+class LiveStoppable(LiveState):
+    def __init__(self, dM):
+        super(LiveStoppable, self).__init__(dM)
+        self.name = "LiveStoppable"
+        self.transitions = {State.EVENTS.STOP : LiveStartable}
+    
+    def run(self, dataModel):
+        super(LiveStoppable, self).run(dataModel)
+        self.dataModel.runEnabled = False
+        self.dataModel.stopEnabled = True
+        self.dataModel.allUIControlsEnabled = False
+
+
+#Offline state classes
+####
+class OfflineState(State):
+    def __init__(self, dM):
+        super(OfflineState, self).__init__()
+    
+    def run(self, dataModel):
+        dataModel.mode = (0, 1)
+        dataModel.usdsControlsEnabled = True
+
+class OfflineSetup(OfflineState):
+    def __init__(self):
+        super(OfflineSetup, self).__init__()
+        self.name = "OfflineSetup"
+        self.transitions = {State.EVENTS.LIVE     : LiveSetup,
+                            State.EVENTS.DATAGOOD : OfflineStartable}
+    
+    def run(self, dataModel):
+        super(OfflineSetup, self).run(dataModel)
+        dataModel.runEnabled = False
+
+class OfflineStartable(OfflineState):
+    def __init__(self):
+        super(OfflineStartable, self).__init__()
+        self.name = "OfflineStartable"
+        self.transitions = {State.EVENTS.DATABAD : OfflineSetup,
+                            State.EVENTS.LIVE    : LiveStartable,
+                            State.EVENTS.RUN     : OfflineStoppable}
+    
+    def run(self, dataModel):
+        super(OfflineStartable, self).run()
+        dataModel.runEnabled = True
+        dataModel.stopEnabled = False
+        dataModel.allUIControlsEnabled = False
+
+class OfflineStoppable(OfflineState):
+    def __init__(self):
+        super(OfflineStoppable, self).__init__()
+        self.name = "OfflineStoppable"
+        self.transitions = {State.EVENTS.STOP : OfflineStoppable}
+    
+    def run(self, dataModel):
+        super(OfflineStoppable, self).run(dataModel)
+        dataModel.runEnabled = False
+        dataModel.stopEnabled = True
+        dataModel.allUIControlsEnabled = False

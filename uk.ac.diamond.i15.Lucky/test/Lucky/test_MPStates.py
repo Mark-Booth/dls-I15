0271f3b00deb80606ad1edde104002f1f8947b5d
Michael Wharmby <michael.wharmby@diamond.ac.uk>
2015-11-11 18:18:54 +0000

    Adds tests for Offline state classes

--- uk.ac.diamond.i15.Lucky/test/Lucky/test_MPStates.py
+++ uk.ac.diamond.i15.Lucky/test/Lucky/test_MPStates.py
@@ -92,6 +92,77 @@ class LiveStoppableTest(MPStateTest):
         
         self.assertEqual(self.dM.mode, (1, 0), "LiveStoppable: Live mode not set")
         self.assertFalse(self.dM.usdsControlsEnabled, "LiveStoppable: US/DS controls enabled")
-        self.assertFalse(self.dM.runEnabled, "LiveStoppable: Run control disabled")
-        self.assertTrue(self.dM.stopEnabled, "LiveStoppable: Stop control enabled")
-        self.assertFalse(self.dM.allUIControlsEnabled, "LiveStoppable: UI controls disabled")
+        self.assertFalse(self.dM.runEnabled, "LiveStoppable: Run control enabled")
+        self.assertTrue(self.dM.stopEnabled, "LiveStoppable: Stop control disabled")
+        self.assertFalse(self.dM.allUIControlsEnabled, "LiveStoppable: UI controls enabled")
+
+class OfflineSetupTest(MPStateTest):
+    def runTest(self):
+        #Force opposite settings to those used in LiveSetup
+        self.dM.mode = (1, 0)
+        self.dM.usdsControlsEnabled = False
+        self.dM.runEnabled = True
+        self.dM.stopEnabled = True
+        self.dM.allUIControlsEnabled = False
+        
+        self.state = OfflineSetup()
+        
+        #Check the correct transitions are present
+        expectedTransitions = [State.EVENTS.DATAGOOD, State.EVENTS.LIVE]
+        self.compareTransitions(expectedTransitions)
+        
+        #Do the state's action
+        self.stateRun()
+        
+        self.assertEqual(self.dM.mode, (0, 1), "OfflineSetup: Live mode not set")
+        self.assertTrue(self.dM.usdsControlsEnabled, "OfflineSetup: US/DS controls disabled")
+        self.assertFalse(self.dM.runEnabled, "OfflineSetup: Run control enabled")
+        self.assertFalse(self.dM.stopEnabled, "OfflineSetup: Stop control enabled")
+        self.assertTrue(self.dM.allUIControlsEnabled, "OfflineSetup: UI controls disabled")
+
+
+class OfflineStartableTest(MPStateTest):
+    def runTest(self):
+        #Force opposite settings to those used in LiveSetup
+        self.dM.mode = (1, 0)
+        self.dM.usdsControlsEnabled = False
+        self.dM.runEnabled = False
+        self.dM.stopEnabled = True
+        self.dM.allUIControlsEnabled = False
+        
+        self.state = OfflineStartable()
+        
+        #Check the correct transitions are present
+        expectedTransitions = [State.EVENTS.DATABAD, State.EVENTS.LIVE, State.EVENTS.RUN]
+        self.compareTransitions(expectedTransitions)
+        
+        self.stateRun()
+        
+        self.assertEqual(self.dM.mode, (0, 1), "OfflineStartable: Live mode not set")
+        self.assertTrue(self.dM.usdsControlsEnabled, "OfflineStartable: US/DS controls disabled")
+        self.assertTrue(self.dM.runEnabled, "OfflineStartable: Run control disabled")
+        self.assertFalse(self.dM.stopEnabled, "OfflineStartable: Stop control enabled")
+        self.assertTrue(self.dM.allUIControlsEnabled, "OfflineStartable: UI controls disabled")
+
+class OfflineStoppableTest(MPStateTest):
+    def runTest(self):
+        #Force opposite settings to those used in LiveSetup
+        self.dM.mode = (1, 0)
+        self.dM.usdsControlsEnabled = False
+        self.dM.runEnabled = True
+        self.dM.stopEnabled = False
+        self.dM.allUIControlsEnabled = True
+        
+        self.state = OfflineStoppable()
+        
+        #Check the correct transitions are present
+        expectedTransitions = [State.EVENTS.STOP]
+        self.compareTransitions(expectedTransitions)
+        
+        self.stateRun()
+        
+        self.assertEqual(self.dM.mode, (0, 1), "OfflineStoppable: Live mode not set")
+        self.assertTrue(self.dM.usdsControlsEnabled, "OfflineStoppable: US/DS controls disabled")
+        self.assertFalse(self.dM.runEnabled, "OfflineStoppable: Run control enabled")
+        self.assertTrue(self.dM.stopEnabled, "OfflineStoppable: Stop control disabled")
+        self.assertFalse(self.dM.allUIControlsEnabled, "OfflineStoppable: UI controls enabled")

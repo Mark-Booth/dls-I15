0a3eda3ffdacdc4053de80d0f2a526886f143ae5
Michael Wharmby <michael.wharmby@diamond.ac.uk>
2015-11-12 11:39:59 +0000

    Adding testing of state change control in main presenter

--- uk.ac.diamond.i15.Lucky/test/Lucky/test_MainPresenter.py
+++ uk.ac.diamond.i15.Lucky/test/Lucky/test_MainPresenter.py
@@ -30,27 +30,26 @@ class ModeTransitionTest(MainPresenterTest):
         self.assertEqual(self.mp.getModeTransition(), State.EVENTS.LIVE, "Expected live, got offline transition")
         
 class RunStopPressActiveChangesTest(MainPresenterTest):
-    pass
-#     def runTest(self):
-#         #Set allDataPresent
-#         self.mp.dataModel.allDataPresent = True
-#         self.assertFalse(self.mp.dataModel.runEnabled, 'Run should be disabled by default')
-#         self.assertFalse(self.mp.dataModel.stopEnabled, 'Stop should be disabled by default')
-#         
-#         #Activate run by toggling
-#         self.mp.toggleButtonStates()
-#         self.assertTrue(self.mp.dataModel.runEnabled, 'Run should be enabled with all data present')
-#         self.assertFalse(self.mp.dataModel.stopEnabled, 'Stop should be disabled with all data present')
-#         
-#         #Activate stop, deactivate run
-#         self.mp.toggleButtonStates()
-#         self.assertFalse(self.mp.dataModel.runEnabled, 'Run should be disabled after starting')
-#         self.assertTrue(self.mp.dataModel.stopEnabled, 'Stop should be enabled after starting')
-#         
+    def runTest(self):
+        #Set allDataPresent
+        self.mp.dataModel.allDataPresent = True
+        self.assertFalse(self.mp.dataModel.runEnabled, 'Run should be disabled by default')
+        self.assertFalse(self.mp.dataModel.stopEnabled, 'Stop should be disabled by default')
+         
+        #Activate run by toggling
+        self.mpSM.changeState(State.EVENTS.DATAGOOD)
+        self.assertTrue(self.mp.dataModel.runEnabled, 'Run should be enabled with all data present')
+        self.assertFalse(self.mp.dataModel.stopEnabled, 'Stop should be disabled with all data present')
+         
+        #Activate stop, deactivate run
+        self.mpSM.changeState(State.EVENTS.RUN)
+        self.assertFalse(self.mp.dataModel.runEnabled, 'Run should be disabled after starting')
+        self.assertTrue(self.mp.dataModel.stopEnabled, 'Stop should be enabled after starting')
+         
 #         with self.assertRaises(BadModelStateException):
 #             self.mp.dataModel.allDataPresent = False
 #             self.mp.toggleButtonStates()
-#         
+#          
 #         #Toggle again and unset allDataPresent
 #         self.mp.dataModel.allDataPresent = True
 #         self.mp.toggleButtonStates()

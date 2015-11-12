5c9dfc02477e388b485970e80bd78f7691f6297e
Michael Wharmby <michael.wharmby@diamond.ac.uk>
2015-11-12 11:26:39 +0000

    Modified test to encapsulate the data object inside a mock main presenter (new class).

--- uk.ac.diamond.i15.Lucky/test/Lucky/test_StateMachine.py
+++ uk.ac.diamond.i15.Lucky/test/Lucky/test_StateMachine.py
@@ -11,11 +11,16 @@ from Lucky.DataModel import MainData
 class FSMTest(unittest.TestCase):
     def setUp(self):
         self.dM = MainData()
-        self.fsm = StateMachine(self.dM)
+        self.mp = MockMainPresenter(self.dM)
+        self.fsm = StateMachine(self.mp)
 
     def tearDown(self):
         self.dM = None
         self.fsm = None
+        
+class MockMainPresenter(object):
+    def __init__(self, dM):
+        self.dataModel = dM
 
 class GetStateTest(FSMTest):
     def runTest(self):
